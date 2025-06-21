56e67474cf4d90a022fcd61bbafb03949c8a2889
Rasik <vlad.fokin2004.vf@gmail.com>
2025-06-21 13:02:12 +0300

    chore: message styles

--- widget-vite/src/components/chatbot-prompt.tsx
+++ widget-vite/src/components/chatbot-prompt.tsx
@@ -6,7 +6,7 @@ export default function ChatbotPrompt({prompt, handleSendMessage, theme}: {promp
         handleSendMessage(prompt);
     }
     return (
-        <div onClick={handleClick} className={`${getStyle(theme, 'prompt')} text-sm rounded-full transition hover:scale-98 cursor-pointer px-4 py-2`}>
+        <div onClick={handleClick} className={`${getStyle(theme, 'prompt')} ai-chatbot-prompt text-sm rounded-full transition hover:scale-98 cursor-pointer px-4 py-2`}>
             {prompt}
         </div>
     );
